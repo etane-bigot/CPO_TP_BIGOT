8b4e14942f287a755ca7f8d62e1938beec4299a5
etane bigot <etane.bigot@epfedu.fr>
2024-09-25 15:31:11 +0200

    TP1 temperature

--- TP1_convertisseur_BIGOT/src/tp1_convertisseur_bigot/TP1_convertisseur_BIGOT.java
+++ TP1_convertisseur_BIGOT/src/tp1_convertisseur_bigot/TP1_convertisseur_BIGOT.java
@@ -13,11 +13,11 @@ public class TP1_convertisseur_BIGOT {
      * @param args the command line arguments
      */
     public static double CelciusVersKelvin (double tCelcius) {
-        tCelcius+=273.15;
+        tCelcius=tCelcius+273.15;
         return tCelcius;
     }
     public static double KelvinVersCelcius (double tKelvin){
-        tKelvin-=273.15;
+        tKelvin=tKelvin-273.15;
         return tKelvin;    
     }
     public static double FarenheitVersCelcius (double tFar){
@@ -40,20 +40,45 @@ public class TP1_convertisseur_BIGOT {
     }
     public static void main(String[] args) {
         double val1;
-        double kelv;
+        double fin;
+        int conv;
          Scanner sc = new Scanner(System.in);
-         System.out.println("System.out.println")
+         System.out.println("Saisir la conversion");
          System.out.println("1) De Celcius vers Kelvin");
-         System.out.println("saisir une valeur");
-         System.out.println("saisir une valeur");
-         System.out.println("saisir une valeur");
-         System.out.println("saisir une valeur");
-         System.out.println("saisir une valeur");
+         System.out.println("2) Kelvin Vers Celcius");
+         System.out.println("3) Farenheit Vers Celcius");
+         System.out.println("4) Celcius Vers Farenheit");
+         System.out.println("5) Kelvin Vers Farenheit");
+         System.out.println("6) De Farenheit vers Kelvin");
+         conv = sc.nextInt();
+         System.out.println("Saisir la valeur que vous souhaiter convertir");
          val1 = sc.nextDouble();
-         System.out.println("celcius : "+val1+"");
-         kelv =CelciusVersKelvin(val1);
-         System.out.println("kelvin : "+kelv+"");
-         // TODO code application logic here
+         if (conv==1){
+             fin=CelciusVersKelvin (val1);
+             System.out.println(""+val1+" degres celcius est egal a "+fin+" degres Kelvin");
+         }
+         else if (conv==2){
+             fin=KelvinVersCelcius (val1);
+             System.out.println(""+val1+" degres Kelvin est egal a "+fin+" degres celcius");
+         }
+         else if (conv==3){
+             fin=FarenheitVersCelcius (val1);
+             System.out.println(""+val1+" degres Farenheit est egal a "+fin+" degres celcius");
+         }
+         else if (conv==4){
+             fin=CelciusVersFarenheit (val1);
+             System.out.println(""+val1+" degres Celcius est egal a "+fin+" degres Farenheit");
+         }
+         else if (conv==5){
+             fin=KelvinVersFarenheit (val1);
+             System.out.println(""+val1+" degres Kelvin est egal a "+fin+" degres Farenheit");
+         }
+         else if (conv==6){
+             fin=FarenheitVersKelvin (val1);
+             System.out.println(""+val1+" degres Farenheit est egal a "+fin+" degres Kelvin");
+         }
+
     }
+         
     
 }
