c1cf1ebf7e20ebf437c837ca7f3499a34e3b7026
etane bigot <etane.bigot@epfedu.fr>
2024-10-16 10:46:42 +0200

    TP2 classe tartiflette

---
+++ TP2_manip_BIGOT/src/tp2_manip_bigot/Tartiflette.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_bigot;
+
+/**
+ *
+ * @author etane
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette(int k){
+        this.nbCalories=k;
+    }
+    
+}
